4f276d492610125a90a17bfab18275c78210963a
Bryan-Steph <frusteveakonnibryan@gmail.com>
2025-04-11 07:55:16 +0100

    Successful creation of the desktop view of the Navigation bar of the E-commerce website AfriMart

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,57 +1,132 @@
-import React, { useState } from "react"
-import { NavbarMenu } from "../mockData/data"
-import { CiSearch } from "react-icons/ci"
-import { FaDumbbell } from "react-icons/fa"
-import { PiShoppingCartThin } from "react-icons/pi"
-import ResponsiveMenu from "./ResponsiveMenu"
+import React from "react";
+import { CiHeart, CiSearch } from "react-icons/ci";
+import { FaDumbbell } from "react-icons/fa";
+import { PiShoppingCartThin } from "react-icons/pi";
 import { TbLayoutSidebarLeftExpandFilled } from "react-icons/tb";
-import { FaDropbox } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 import { GiWrappingStar } from "react-icons/gi";
 import { FaFire } from "react-icons/fa6";
+import { MdOutlineWifiTetheringErrorRounded } from "react-icons/md";
+import { CiUser } from "react-icons/ci";
 
 
-function Navbar(){
-        // const [open, setOpen] = useState(false);
 
-    return(<>
-    <nav>
-        <div className=" flex justify-evenly gap- py-6 ">
-            {/* sidebar */}
-            <TbLayoutSidebarLeftExpandFilled  className="ml-0 text-4xl text-primary "/>
-            <button className="bg-secondary p-2 rounded-lg w-ful flex text-white">
-                Browse all categories
-                <IoIosArrowDown className="pt-1 text-2xl  "/>
-            </button>
-            {/* Menu section */}
-            <div className="hidden md:block " id="nav-menu">
-                <ul className="flex items-center gap-6 text-gray-600">
-                   <li><a href="" className=" py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"> <FaFire className="mt-[2px] text-secondary"/> Deals</a></li>
-                   <li><a href="" className="inline-block py-1 px-3 hover:text-secondary capitalize font-semibold"> Home</a></li>
-                   <li><a href="" className=" py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1 "> Shop < IoIosArrowDown className="pt-1"/></a></li>
-                   <li><a href="" className=" py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1 "> Product < IoIosArrowDown className="pt-1"/></a></li>
-                   <li><a href="" className="inline-block py-1 px-3 hover:text-secondary capitalize font-semibold"> About</a></li>
-                   <li><a href="" className=" py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1 "> Vendors < IoIosArrowDown className="pt-1"/></a></li>
-                   <li><a href="" className=" py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1 "> Pages < IoIosArrowDown className="pt-1"/></a></li>
-                </ul>
-            </div>
-            {/* Icons section */}
-            <div className="flex items-center gap-4 ">
-           
-            <button className=" text-secondary font-semibold
-            hover:text-primary rounded-md  px-6 oy-2 transition-all
-            duration-200 hidden md:flex md:gap-2 ">
-                <GiWrappingStar/>
-                Save 10% off your first purchase</button>
-            </div>
-          
-            {/* Mobile hamburger Menu section */}
-           
-        </div>
-    </nav>
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <div className="flex flex-col md:flex-row justify-evenly gap-4 py-6">
+                    {/* Sidebar */}
+                    <TbLayoutSidebarLeftExpandFilled className="ml-0 text-4xl text-primary" />
+                    <button className="bg-secondary p-2 rounded-lg  flex text-white">
+                        Browse all categories
+                        <IoIosArrowDown className="pt-1 text-2xl" />
+                    </button>
 
-    {/* Mobile sidebar section */}
-    </>)
+                          {/* SEARCH SECTION */}
+                    <div className="flex items-center md:ml-[200px] border-2 border-primary rounded-lg w-full md:max-w-[600px]">
+                        <input
+                            type="text"
+                            placeholder="Search for items..."
+                            className="md:w-full w-full md:px-4 px-2 py-2 outline-none text-gray-700"
+                        />
+                        <button className="px-4">
+                            <CiSearch className="text-primary md:text-2xl" />
+                        </button>
+                    </div>
+
+                        <section className="flex md:gap-3">
+                        <button
+                            className="flex md:ml-[200px] hover:bg-primary hover:text-white rounded-full p-2 duration-200"
+                        >
+                            <MdOutlineWifiTetheringErrorRounded className="text-3xl" />
+                            <p className="pt-1">Compare</p>
+                        </button>
+                        <button
+                            className="flex hover:bg-primary hover:text-white rounded-full p-2 duration-200"
+                        >
+                            <CiHeart className="text-3xl" />
+                            <p className="pt-1">Wishlist</p>
+                        </button>
+                        <button
+                            className="flex hover:bg-primary hover:text-white rounded-full p-2 duration-200"
+                        >
+                            <PiShoppingCartThin className="text-3xl" />
+                            <p className="pt-1">Cart</p>
+                        </button>
+                        <button
+                            className="flex hover:bg-primary hover:text-white rounded-full p-2 duration-200"
+                        >
+                            <CiUser className="text-3xl" />
+                            <p className="pt-1">Account</p>
+                        </button>
+                    </section>
+
+                    {/* Menu section */}
+                    {/* <div className="block" id="nav-menu">
+                        <ul className="flex flex-col md:flex-row items-center gap-6 text-gray-600">
+                            <li>
+                                <a
+                                    href=""
+                                    className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                >
+                                    <FaFire className="mt-[2px] text-secondary" /> Deals
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href=""
+                                    className="inline-block py-1 px-3 hover:text-secondary capitalize font-semibold"
+                                >
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href=""
+                                    className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                >
+                                    Shop <IoIosArrowDown className="pt-1" />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href=""
+                                    className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                >
+                                    Product <IoIosArrowDown className="pt-1" />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href=""
+                                    className="inline-block py-1 px-3 hover:text-secondary capitalize font-semibold"
+                                >
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href=""
+                                    className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                >
+                                    Vendors <IoIosArrowDown className="pt-1" />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href=""
+                                    className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                >
+                                    Pages <IoIosArrowDown className="pt-1" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div> */}
+                </div>
+            </nav>
+        </>
+    );
 }
 
-export default Navbar
+export default Navbar;    
--- src/Components/NavbarOne.jsx
+++ src/Components/NavbarOne.jsx
@@ -1,67 +1,122 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { CiHeart, CiSearch } from "react-icons/ci";
-// import { CiHeart } from "react-icons/fa"
-import { CiUser } from "react-icons/ci"
-import { PiShoppingCartThin } from "react-icons/pi"
-import { MdMenu } from "react-icons/md"
+import { PiShoppingCartThin } from "react-icons/pi";
+import { MdMenu } from "react-icons/md";
+import { IoMdClose } from "react-icons/io"; // Import close icon
+import Navbar from "./Navbar"; // Import Navbar to display its nav-menu
+import { FaFire } from "react-icons/fa6";
+import { IoIosArrowDown } from "react-icons/io";
 
 
-import { MdOutlineWifiTetheringErrorRounded } from "react-icons/md";
 
-function NavbarOne(){
+function NavbarOne() {
+    // const [isNavVisible, setIsNavVisible] = useState(false); 
 
+    // const toggleNav = () => {
+    //     setIsNavVisible(!isNavVisible);
+    // };
 
-    return(
+    return (
         <>
-        <div className="md:flex block  gap-5 mt-5 bg-white shadow-md p-2 rounded-lg">       
-            <div className="flex items-center justify-between">
-            <h1 className="text-3xl font-semibold md:ml-10">Afri<span className="text-primary
-            ">Mart</span></h1>
-            <select name="" id="" className="md:ml-10 hidden md:block p-2 bg-primary text-white rounded-lg"> 
-                <option value="">Location</option>
-                <option value="">Bamenda</option>
-            </select>
-            <div className="md:hidden">
-            <MdMenu className=" text-4xl  "  />
+            {/* Sticky Navbar */}
+            <div className="sticky top-0 z-50 bg-white shadow-md">
+                <div className="md:flex block gap-5 mt-5 p-2 rounded-lg">
+                    <div className="flex items-center justify-between">
+                        <h1 className="text-3xl font-semibold md:ml-10">
+                            Afri<span className="text-primary">Mart</span>
+                        </h1>
+                        <select
+                            name=""
+                            id=""
+                            className="md:ml-10 hidden md:block p-2 bg-primary text-white rounded-lg"
+                        >
+                            <option value="">Location</option>
+                            <option value="">Bamenda</option>
+                        </select>
+                        <div className="md:hidden">
+                            <MdMenu className="text-4xl cursor-pointer"  />
+                        </div>
+                    </div>
+              
+
+                         {/* Menu section */}
+                                        <div className="block md:ml-[200px]" id="nav-menu">
+                                            <ul className="flex flex-col md:flex-row items-center gap-6 text-gray-600">
+                                                <li>
+                                                    <a
+                                                        href=""
+                                                        className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                                    >
+                                                        <FaFire className="mt-[2px] text-secondary" /> Deals
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href=""
+                                                        className="inline-block py-1 px-3 hover:text-secondary capitalize font-semibold"
+                                                    >
+                                                        Home
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href=""
+                                                        className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                                    >
+                                                        Shop <IoIosArrowDown className="pt-1" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href=""
+                                                        className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                                    >
+                                                        Product <IoIosArrowDown className="pt-1" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href=""
+                                                        className="inline-block py-1 px-3 hover:text-secondary capitalize font-semibold"
+                                                    >
+                                                        About
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href=""
+                                                        className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                                    >
+                                                        Vendors <IoIosArrowDown className="pt-1" />
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href=""
+                                                        className="py-1 px-3 hover:text-secondary capitalize font-semibold flex gap-1"
+                                                    >
+                                                        Pages <IoIosArrowDown className="pt-1" />
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                    
+                                        <section className="flex md:ml-[200px]">
+                                            <div className="flex">
+                                                <p className="flex font-semibold"><CiHeart className="pt-1 "/> login or register</p>
+                                                <p className="md:ml-5 font-semibold">Hot line: 683 538 004</p>
+                                            </div>
+
+                                             </section>
+
+                 
+                </div>
             </div>
-            </div>
-         <div class="flex items-center md:ml-[200px] border-2 border-primary rounded-lg  w-full md:max-w-[600px]">
 
-            <input type="text" placeholder="Search for items..." className="md:w-full w-full md:px-4 px-2 py-2 outline-none text-gray-700"/>
-
-            <button class="px-4">
-                    <CiSearch className="text-primary md:text-2xl"/>
-            </button>
-         </div>
-
-         <section className="flex md:gap-3">
-               <button className="flex md:ml-[200px]
-             hover:bg-primary hover:text-white rounded-full
-            p-2 duration-200">
-            <MdOutlineWifiTetheringErrorRounded className="text-3xl"/>
-            <p  className="pt-1">Compare</p>
-            </button>
-            <button className="flex  hover:bg-primary hover:text-white rounded-full
-            p-2 duration-200">
-                <CiHeart className="text-3xl" />
-                <p className="pt-1">wishlist</p>
-            </button>
-            <button className="flex  hover:bg-primary hover:text-white rounded-full
-            p-2 duration-200">
-                    < PiShoppingCartThin className="text-3xl" />
-                    <p className="pt-1">Cart</p>
-            </button>
-            <button className="flex  hover:bg-primary hover:text-white rounded-full
-            p-2 duration-200">
-                    <CiUser className="text-3xl" />
-                    <p className="pt-1">Account</p>
-            </button>
-            </section>
-         
-           
-        </div>
+      
+        
         </>
-    )
+    );
 }
 
-export default NavbarOne
+export default NavbarOne;
